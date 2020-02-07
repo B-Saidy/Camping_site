068745c49b47c2547896985dc962099cbe5beeea
B-Saidy <baijaw93@yahoo.com>
2020-02-08 01:01:30 +0800

    added django_heroku

--- yelpcamp/settings.py
+++ yelpcamp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -145,3 +146,4 @@ DEFAULT_FILE_STORAGE = 'yelpcamp.custom_storage.MediaStorage'
 AWS_S3_REGION_NAME = 'us-east-2' #change to your region
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
+django_heroku.settings(locals())
